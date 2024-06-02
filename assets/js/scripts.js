1ce8d0a721d51d4a8f21bcfbff2d9ca769003652
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-06-02 10:03:43 -0700

    fixed bug with clearing weather box of previous city search

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@ const searchCityButton= document.querySelector("#citi-button");
 const holderCities = document.querySelector("#cities");
 const citiesList = document.querySelector("#cities-list");
 const currentWeather = document.querySelector("#input-area");
-const box = document.querySelector("weather-box");
+let bodyBox = document.querySelector("#weather-box");
 
 const cityInput = document.querySelector("#search-city")
 const APIkey = "a982c70229a3cc2a4eb22edd33dd6ff6";
@@ -58,7 +58,7 @@ function getWeatherApi(city){
     .then(function(response){
         console.log(response.status);
         response.json().then(function(data){
-        console.log(data);
+        console.log(data);      
         outputCurrentReport(data);
 
     })
@@ -77,8 +77,6 @@ function convertToDate(seconds){
 
 function outputCurrentReport(value){
 
-    
-
 let bodyBox = document.createElement("div");
 bodyBox.id ="weather-box";
 let headerEl = document.createElement("h1");
@@ -96,7 +94,7 @@ let humidityValue = document.createElement("p");
 humidityValue.textContent = `Humidity ${value.main.humidity}`
 
 
-bodyBox.append(headerEl, iconImg,tempValue,windValue,humidityValue);
+bodyBox.append(headerEl,iconImg,tempValue,windValue,humidityValue);
 bodyBox.setAttribute("class", "ms-5 col-8 border border-dark inline")
 
 currentWeather.appendChild(bodyBox);
@@ -123,11 +121,13 @@ function findForecast(event){
         cityInput.value ="";
         return;
     }
-
+     
+    let box = document.querySelector("#weather-box");
     if(box){
-        box.innerHTML = "";
-
+        currentWeather.removeChild(box);
     }
+    // currentWeather.removeChild(box);
+
     storeValue(cityInput.value);
     getWeatherApi(cityInput.value)
     console.log(cityInput.value);
